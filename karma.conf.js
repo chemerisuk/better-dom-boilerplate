c81499813ef4f090d110018ff00cdd5ddffb4479
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-10-15 21:17:42 +0300

    add karmaFiles parameter support

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,15 @@
 module.exports = function(config) {
     "use strict";
 
+    var pkg = require("../../package");
+    var files = pkg.karmaFiles || ["bower_components/better-dom/dist/better-dom.js"];
+
     config.set({
         basePath: "../../",
         singleRun: true,
         frameworks: ["jasmine"],
         browsers: ["PhantomJS"],
         preprocessors: { "build/*.js": "coverage" },
-        files: [
-            "bower_components/better-dom/dist/better-dom.js",
-            "build/*.js",
-            "test/*.spec.js"
-        ]
+        files: files.concat("build/*.js", "test/*.spec.js")
     });
 };
