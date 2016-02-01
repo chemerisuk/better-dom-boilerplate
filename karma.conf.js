ef643adbcd8bab14ec0bd33f24ed8f6e2045e6c9
Maksim Chemerisuk <chemerisuk@gmail.com>
2016-02-02 02:19:24 +0300

    add support for babel in karma

--- karma.conf.js
+++ karma.conf.js
@@ -2,13 +2,16 @@ module.exports = function(config) {
     "use strict";
 
     var pkg = require("../../package");
+    var babelConfig = require.resolve("./.babelrc");
     var files = pkg.karmaFiles || ["bower_components/better-dom/dist/better-dom.js"];
 
     config.set({
         basePath: "../../",
         singleRun: true,
         frameworks: ["jasmine-ajax", "jasmine"],
-        plugins: ["karma-jasmine-ajax", "karma-phantomjs-launcher", "karma-jasmine", "karma-coverage", "karma-coveralls"],
+        preprocessors: {"test/*.spec.js": ["babel"]},
+        babelPreprocessor: {"extends": babelConfig},
+        plugins: ["karma-jasmine-ajax", "karma-phantomjs-launcher", "karma-jasmine", "karma-coverage", "karma-coveralls", "karma-babel-preprocessor"],
         browsers: ["PhantomJS"],
         files: files.concat("build/*.js", "test/*.spec.js")
     });
